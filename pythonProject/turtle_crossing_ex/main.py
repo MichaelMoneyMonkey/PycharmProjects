33a3ce531065c929b8c6e263ab4764fe9c9b4f08
MichaelMoneyMonkey <michael.b@moneymonkeyproductions.com>
2022-05-13 16:42:42 +0200

    todo discript + start exercise

---
+++ pythonProject/turtle_crossing_ex/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+import time
+
+
+
+player = Player()
+car_manager = CarManager
+scoreboard = Scoreboard
