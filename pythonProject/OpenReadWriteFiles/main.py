f7a47cb6d580060b26e4d69792320a103efa6b16
MichaelMoneyMonkey <michael.b@moneymonkeyproductions.com>
2022-05-31 15:35:02 +0200

    .

--- pythonProject/OpenReadWriteFiles/main.py
+++ pythonProject/OpenReadWriteFiles/main.py
@@ -1,1 +1,19 @@
-file = open("my_file.txt")
+# # optie 1
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+#
+# # optie 2
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# mode="w" = write
+# mode="a" = append
+# if file is on desktop
+with open("../../../../Users/Robby/Desktop/new_file.txt", mode="w") as file:
+    file.write("\n New tefsdxt.")
+
+# C:/Github/PycharmProjects/pythonProject/OpenReadWriteFiles/main.py
--- pythonProject/snake_game/scoreboard.py
+++ pythonProject/snake_game/scoreboard.py
@@ -6,7 +6,10 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+
         self.color("white")
         self.penup()
         self.goto(x=0, y=250)
@@ -22,6 +25,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
 
         self.score = 0
         self.update_scoreboard()
